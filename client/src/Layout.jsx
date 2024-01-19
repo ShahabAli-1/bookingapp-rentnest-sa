593a1c9eaac4e825cfd0008177712fa0d27dae74
ShahabAli-1 <88381344+ShahabAli-1@users.noreply.github.com>
2024-01-19 06:39:56 -0800

    Few frontend fixes

--- client/src/Layout.jsx
+++ client/src/Layout.jsx
@@ -9,7 +9,7 @@ const Layout = () => {
       <Outlet />
       <div className="fixed bottom-0 left-0 right-0 flex justify-center mb-4">
         <button className="bg-blue-500 text-white px-4 py-2 rounded-full">
-          Sticky Button
+          Home
         </button>
       </div>
     </div>
