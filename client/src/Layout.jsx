67efbf37b338f306f4757d09a8621dfabdb3b5ea
ShahabAli-1 <88381344+ShahabAli-1@users.noreply.github.com>
2024-01-19 06:39:32 -0800

    Few frontend fixes

--- client/src/Layout.jsx
+++ client/src/Layout.jsx
@@ -7,6 +7,11 @@ const Layout = () => {
     <div className="py-4 px-8 flex flex-col min-h-screen">
       <Header />
       <Outlet />
+      <div className="fixed bottom-0 left-0 right-0 flex justify-center mb-4">
+        <button className="bg-blue-500 text-white px-4 py-2 rounded-full">
+          Sticky Button
+        </button>
+      </div>
     </div>
   );
 };
