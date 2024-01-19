ad9dc6dc78c62b93c4a7f24a9cda5a9ce5424bd9
ShahabAli-1 <88381344+ShahabAli-1@users.noreply.github.com>
2024-01-19 06:46:47 -0800

    Few frontend fixes

--- client/src/Layout.jsx
+++ client/src/Layout.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import Header from "./components/Header";
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 const Layout = () => {
   return (
     <div className="py-4 px-8 flex flex-col min-h-screen">
       <Header />
       <Outlet />
+
       <div className="fixed bottom-0 left-0 right-0 flex justify-center mb-4">
-        <button className="bg-blue-500 text-white px-4 py-2 rounded-full">
-          Home
-        </button>
+        <Link>
+          <button className="bg-primary text-white px-4 py-2 rounded-full">
+            Home
+          </button>
+        </Link>
       </div>
     </div>
   );
